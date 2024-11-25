5a4e3606aa671c6ef8112b11ebd483244ea25d44
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-11-25 18:40:07 +0200

    install

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Aos from 'aos/src/js/aos.js';
+import '@/js/loader.js';
+Aos.init();
+// import '@/js/iziToast.js';
+// import '@/js/slider.js';
+// import '@/js/header.js';
+// import '@/js/hero.js';
+// import '@/js/about-me.js';
+// import '@/js/skills-list.js';
+// import '@/js/benefits.js';
+// import '@/js/projects.js';
+// import '@/js/faq.js';
+// import '@/js/covers.js';
+// import '@/js/reviews.js';
+// import '@/js/work-together.js';
+// import '@/js/modal.js';
+// import '@/js/theme-swither.js';
